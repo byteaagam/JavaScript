5033255be22d9c787130c832ba7a5be3a1e474d3
jaagam <76506786+jaagam@users.noreply.github.com>
2025-12-04 04:35:35 +0000

    comparison of data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,7 @@
 let score= '33'
-console.log(tyeof score)
+//console.log(typeof score)
 let valueInNumber= Number(score)
-console.log(typeof valueInNumber)
+//console.log(typeof valueInNumber)
 
 
 // "33"=> 33
@@ -10,12 +10,30 @@ console.log(typeof valueInNumber)
 
 let isLoggedIn= 1
 let booleanIsLoggedIn= Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 // 1=> true; 0=> false
 // ""=> false; "abc"=> true
 
 let someNumber=33
 let stringNumber= String(someNumber)
-console.log(typeof stringNumber)
+//console.log(typeof stringNumber)
+
 //33 => "33"
+
+//***************************************Operations******************************************* */
+
+let value= 3
+let negValue= -value
+//console.log(negValue)
+
+let str1= "Hello"
+str2= " World" 
+
+let str3= str1+str2
+console.log(str3)
+
+console.log;("1" +2) //"12"
+console.log(1+ "2") //"12"
+console.log("1" +2+ 2)// "122"
+console.log(1+ 2+ "2")// "32"
