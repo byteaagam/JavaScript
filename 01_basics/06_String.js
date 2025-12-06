4fe7dd1904ac123fe01349f54321fe7f318e4704
jaagam <76506786+jaagam@users.noreply.github.com>
2025-12-06 04:13:52 +0000

    dk

---
+++ 01_basics/06_String.js
@@ -0,0 +1,28 @@
+const name= "Aagam "
+const repoCount=50
+//console.log(name + repoCount+ " value")
+console.log(`Hello my name is ${name} and my remp count is ${repoCount}`)
+
+const gameName = new String('Aagam-jain-01')
+console.log(gameName[0])
+console.log(gameName.__prto__)
+
+//console.log(gameName.length)
+//console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('g'))
+
+const newString= gameName.substring(0,4)
+// console.log(newString)
+
+const newString1= gameName.slice(-8,4)
+
+const newStringOne= "        Space       "
+// console.log(newStringOne)
+// console.log(newStringOne.trim());
+
+const url = "https://aagam.com/aagam%20jain"
+// console.log(url.replace('%20', '-'))
+// console.log(url.includes('sunder'))
+
+console.log(gameName.split('-'))
